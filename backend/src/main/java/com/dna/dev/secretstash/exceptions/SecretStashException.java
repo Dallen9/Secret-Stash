c01495fa96b39770635cb7fa78e602379b2599de
Donovan <allendonovan@gmail.com>
2020-11-03 20:42:35 -0800

    Production

--- backend/src/main/java/com/dna/dev/secretstash/exceptions/SecretStashException.java
+++
@@ -1,7 +0,0 @@
-package com.dna.dev.secretstash.exceptions;
-
-public class SecretStashException extends RuntimeException {
-    public SecretStashException(String message) {
-        super(message);
-    }
-}
