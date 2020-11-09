139fa5003cb01272b7f67bf473d4bbd1a0937c80
Donovan <allendonovan@gmail.com>
2020-11-08 17:55:31 -0800

    update

--- craco.config.js
+++ craco.config.js
@@ -1,41 +1,17 @@
-const PurgecssPlugin = require('purgecss-webpack-plugin')
-var UglifyJsPlugin = require("uglifyjs-webpack-plugin");
-var HtmlWebpackPlugin = require("html-webpack-plugin");
-const MiniCssExtractPlugin = require("mini-css-extract-plugin");
-const glob = require('glob')
-const path = require('path')
+const purgecss = require('@fullhuman/postcss-purgecss');
 
 module.exports = {
-  webpack: {
-    alias: {
-      react: path.resolve(__dirname, './node_modules/react'),
+  style: {
+    postcss: {
+      plugins: [
+        purgecss({
+          content: ['./src/**/*.html', './src/**/*.jsx', './src/**/*.js'],
+          css: ['./src/App.css'],
+          safelist: ['button', 'segment', 'seg', 'card', 'a', 
+          'body', 'h1', 'h2', 'h3', 'container', 'color', 'style', 'i', 'input', 'form' ]
+        }),
+      ],
     },
-    plugins: [
-      new HtmlWebpackPlugin({
-        template: "./public/index.html"
-      }),
-      new MiniCssExtractPlugin({
-        filename: "[name].css",
-        chunkFilename: "[id].css"
-      }),
-      new PurgecssPlugin({
-        paths: glob.sync(`${path.join(__dirname, 'src')}/**/*`,  { nodir: true }),
-      }),
-    ],
-    configure: {
-      module: {
-        rules: [
-    
-          // css file: extract to css file with mini extract plugin
-          {
-            test: /\.css$/,
-            use: [MiniCssExtractPlugin.loader, "css-loader"]
-          }
-        ]
-      },
-      optimization: {
-        minimizer: [new UglifyJsPlugin()]
-      },
-    }
-  }
-};
+  },
+  
+}
